cc4591d3ba5e693d2bb191deb1178d2a23a7627c
潘逸安 <ppiian@126.com>
2020-10-14 11:10:56 +0800

    init

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-import time
 import datetime
-import pymysql
-from flask_bootstrap import Bootstrap
+
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 from db import Runtime
 
 app = Flask(__name__)
 Bootstrap(app)
 
+
 @app.route('/')
 def hello_world():
-
     items = Runtime.select()
     for item in items:
-        item.is_add = item.run_time.hour > 18
+        # 如果5.30下班，标记一下
+        item.is_add = item.run_time > datetime.time(hour=17, minute=45)
     return render_template('hello.html', items=items)
 
 
--- db.py
+++ db.py
@@ -6,6 +6,7 @@ from peewee import *
 class Runtime(Model):
     run_date = DateField()
     run_time = TimeField()
+
     class Meta:
         setting = {'host': 'localhost', 'user': 'root', 'password': 'rootroot', 'database': 'worktime'}
         database = MySQLDatabase(**setting)
@@ -13,11 +14,10 @@ class Runtime(Model):
     # def set_database(self):
     #     self._meta.database = SqliteDatabase('new_app.db')
 
+
 if __name__ == '__main__':
     all = Runtime.select()
     for item in all:
-        item.is_add = item.run_time.hour > 18
-        print(item.is_add)
-    for item in all:
+        item.is_add = item.run_time > datetime.time(hour=17, minute=30)
         print(item.is_add)
     print(all)
